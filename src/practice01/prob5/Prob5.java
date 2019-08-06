05103f92585ab87b58bfe00124d9e3bd1b0a5728
BIT <BIT@DESKTOP-TJ8UB7F>
2019-08-06 17:31:01 +0900

    문제5

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,5 +2,26 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
+		int i=1;
+		int init=0;
+		while(i<100) {
+			int quo=1;
+			int dis=i;
+			while(quo>0) {						
+				quo = dis/10;
+				dis = dis-quo*10;
+				if((dis==3||dis==6||dis==9)&&init==0) {
+					System.out.println();
+					System.out.print(i);
+					System.out.print("¦");
+					init=1;
+				}else if(dis==3||dis==6||dis==9) {
+					System.out.print("¦");
+				}
+				dis=quo;
+			}
+			i++;
+			init=0;
+		}
 	}
 }
