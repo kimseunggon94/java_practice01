a166021b145fd64bf607d18109b36e308d45ad31
BIT <BIT@DESKTOP-TJ8UB7F>
2019-08-06 16:18:14 +0900

    문제2

--- src/practice01/prob2/Prob2.java
+++ src/practice01/prob2/Prob2.java
@@ -2,5 +2,14 @@ package practice01.prob2;
 
 public class Prob2 {
 	public static void main (String args[]) {
+		for(int i=1;i<6;i++) {
+			int ini =0;
+			for(int j=i;ini<10;ini++) {
+				System.out.print(j+" ");
+				j++;
+			}
+			System.out.println();
+			
+		}
 	}
 }
