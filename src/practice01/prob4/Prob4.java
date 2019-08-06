89bb2e74ddc939fa30b6acc5eee1f1051c30a2ee
BIT <BIT@DESKTOP-TJ8UB7F>
2019-08-06 16:32:53 +0900

    문제4

--- src/practice01/prob4/Prob4.java
+++ src/practice01/prob4/Prob4.java
@@ -1,6 +1,17 @@
 package practice01.prob4;
 
+import java.util.Scanner;
+
 public class Prob4 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		for(int i=0; i<str.length();i++) {
+			for(int j=0; j<i+1;j++) {
+				System.out.print(str.charAt(j));
+			}
+			System.out.println();
+		}
+		
 	}
 }
